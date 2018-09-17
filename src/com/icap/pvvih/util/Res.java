ad8daf1b0bf0df00c7f09d2dc2e9c6ccdd170d80
riddyndoma <riddyndoma@gmail.com>
2018-09-17 10:56:55 +0200

    Init Ressource class!

---
+++ src/com/icap/pvvih/util/Res.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.icap.pvvih.util;
+
+/**
+ *
+ * @author DELL
+ */
+public class Res {
+    public  static final String LOGO_APP="/com/icap/pvvih/res/logo.png";
+    
+}
