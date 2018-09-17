92273a7c89e8b5e3cb2ffaf96225e937d09bdd05
riddyndoma <riddyndoma@gmail.com>
2018-09-17 16:30:45 +0200

    First dashbord done!

--- src/com/icap/pvvih/view/PVVIH.java
+++ src/com/icap/pvvih/view/PVVIH.java
@@ -10,6 +10,7 @@ import com.icap.pvvih.util.Res;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -36,7 +37,7 @@ public class PVVIH extends javax.swing.JFrame {
         initComponents();
         doDefaultConfig(connectedUser);
     }
-
+    
     private void doDefaultConfig(Users currentUser) {
         this.setResizable(false);
         this.setLocationRelativeTo(this);
@@ -45,7 +46,7 @@ public class PVVIH extends javax.swing.JFrame {
         this.setTitle("PVVIH DC 1.0");
         lblConnectedUser.setText(currentUser.getPrenom() + " " + currentUser.getNom() + " connected.");
         tsWelcome.setHorizontalAlignment(SwingConstants.CENTER);
-
+        
         final JXHyperlink lblHome = new JXHyperlink();
         lblHome.setText("Home");
         final JXHyperlink lblDE = new JXHyperlink();
@@ -54,7 +55,7 @@ public class PVVIH extends javax.swing.JFrame {
         lblSignout.setText("Sign out");
         final JXHyperlink lblUser = new JXHyperlink();
         lblUser.setText("User manager");
-
+        
         taskActions.add(lblHome);
         taskActions.add(lblDE);
         taskOthers.add(lblUser);
@@ -64,33 +65,33 @@ public class PVVIH extends javax.swing.JFrame {
         lblHome.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                
             }
         });
-
+        
         lblDE.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                
             }
         });
-
+        
         lblUser.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
+                
             }
         });
-
+        
         lblSignout.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 btnSignoutActionPerformed(e);
             }
         });
-
+        scrollManager.setViewportView(createDemoPanel());
     }
-
+    
     private void doPreConfig() {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -116,6 +117,7 @@ public class PVVIH extends javax.swing.JFrame {
         taskActions = new org.jdesktop.swingx.JXTaskPane();
         taskOthers = new org.jdesktop.swingx.JXTaskPane();
         panCentral = new org.jdesktop.swingx.JXPanel();
+        scrollManager = new javax.swing.JScrollPane();
         tsWelcome = new org.jdesktop.swingx.JXTitledSeparator();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
@@ -150,15 +152,17 @@ public class PVVIH extends javax.swing.JFrame {
         panCentral.setBackground(java.awt.Color.white);
         panCentral.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
 
+        scrollManager.setBackground(java.awt.Color.white);
+
         javax.swing.GroupLayout panCentralLayout = new javax.swing.GroupLayout(panCentral);
         panCentral.setLayout(panCentralLayout);
         panCentralLayout.setHorizontalGroup(
             panCentralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addComponent(scrollManager)
         );
         panCentralLayout.setVerticalGroup(
             panCentralLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addComponent(scrollManager)
         );
 
         tsWelcome.setTitle("Welcome !");
@@ -227,7 +231,30 @@ public class PVVIH extends javax.swing.JFrame {
         this.setVisible(false);
         new Login().setVisible(true);
     }//GEN-LAST:event_btnSignoutActionPerformed
-
+    private static PieDataset createDataset() {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        dataset.setValue("IPhone 5s", new Double(20));        
+        dataset.setValue("SamSung Grand", new Double(20));        
+        dataset.setValue("MotoG", new Double(40));        
+        dataset.setValue("Nokia Lumia", new Double(10));        
+        return dataset;        
+    }
+    
+    private static JFreeChart createChart(PieDataset dataset) {
+        JFreeChart chart = ChartFactory.createPieChart(
+                "Data stored statistics", // chart title 
+                dataset, // data    
+                true, // include legend   
+                true,
+                false);
+        
+        return chart;
+    }
+    
+    public static JPanel createDemoPanel() {
+        JFreeChart chart = createChart(createDataset());        
+        return new ChartPanel(chart);        
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private org.jdesktop.swingx.JXButton btnSignout;
     private javax.swing.JMenu jMenu1;
@@ -237,6 +264,7 @@ public class PVVIH extends javax.swing.JFrame {
     private org.jdesktop.swingx.JXTaskPaneContainer jXTaskPaneContainer1;
     private org.jdesktop.swingx.JXLabel lblConnectedUser;
     private org.jdesktop.swingx.JXPanel panCentral;
+    private javax.swing.JScrollPane scrollManager;
     private org.jdesktop.swingx.JXTaskPane taskActions;
     private org.jdesktop.swingx.JXTaskPane taskOthers;
     private org.jdesktop.swingx.JXTitledSeparator tsWelcome;
